d160973512fc01e55cfdfb50e73faa3bd6e45025
Aadi Bery <aadibery@gmail.com>
2025-08-22 22:02:43 -0700

    updates to notebook + saving of ml model

---
+++ demo.py
@@ -0,0 +1,17 @@
+# install dependencies
+
+# sections of the webpage:
+
+# title and description(explanation of project)
+
+# text input box for new reviews
+
+# showcase predictions + probabilities for each category
+
+# create a SHAP waterfall plot
+
+# option to take in a corpus of text?
+
+# about / methodology
+
+# possibly use flask or streamlit!
