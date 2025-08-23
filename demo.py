50aeea5540307938244518ab85062afc0d8e2a3a
Aadi Bery <aadibery@gmail.com>
2025-08-23 13:43:45 -0700

    updates to web app, defined requirements.txt

--- demo.py
+++ demo.py
@@ -19,41 +19,56 @@ explainer = pickle.load(open('shap_explainer.sav', 'rb'))
 st.header("🎥 Movie Reviews Sentiment Analysis - Demo")
 st.subheader("Created by Aadi Bery")
 
-st.write("This project was originally created as the final project as a part of the Cornell Tech ML Foundations Certification through BTT but expanded further and built on with new techniques and tools to make it more complete")
+st.write("This project was originally created as the final project as a part of the Cornell Tech ML Foundations Certification through BTT but expanded further and built on with new techniques and tools to make it more complete.")
 st.write("This web app allows you to input your own movie review and see whether the model predicts it to be a good or bad review, along with an explanation of the prediction using SHAP values (explainable AI!)")
 st.markdown("This is acheived through a simple logistic regression model with accuracy is **~85%** and **auc score ~92%**. If you are interested I've listed more about this on the [Github README](https://github.com/aadib2/movie-review-sentiment-analyzer)")
 
 # Add some example reviews users can click to try
 st.write("**Try one of these examples to get started or input your own review!**")
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
+# --- RESET LOGIC: must be before st.text_area ---
+if "review_input" not in st.session_state:
+    st.session_state["review_input"] = ""
+
+if st.button("Reset"):
+    st.session_state["review_input"] = ""
+    st.rerun()  # rerun to clear the text box immediately
+
+# if buttons were pressed then prepopulate review
 with col1:
     if st.button("😊 Positive Example"):
-        review_input = "This movie was absolutely fantastic! Amazing acting and brilliant storyline."
+        st.session_state["review_input"] = "This movie was absolutely fantastic! Amazing acting and brilliant storyline."
+        st.rerun()
 
 with col2:
     if st.button("😞 Negative Example"):  
-        review_input = "Terrible movie. Boring plot and awful acting. Complete waste of time."
+        st.session_state['review_input'] = "Terrible movie. Boring plot and awful acting. Complete waste of time."
+        st.rerun()
 
-# Then your text area
+# text input of review
 review_input = st.text_area(
     "📝 Enter your movie review here!", 
-    value=review_input if 'review_input' in locals() else "", # FIGURE OUT WHY BUTTONS AREN'T WORKING
+    value=st.session_state['review_input'],
+    key="review_input",
     placeholder="Type your movie review here...",
     height=150
 )
 
-if(st.button('Predict') and review_input is not None):
-    # perform prediction pipeline
 
-    # we need to first preprocess this review so it is interpretable by the model
+
+# perform prediction pipeline
+if(st.button('Predict') and review_input != ""):
+
+    # we need to first preprocess this review (removing stopwords, punctuation, tokenize, etc.)
     cleaned_review = preprocess(review_input)
+
     # now vectorize this cleaned review using the tfidf vectorizer we fit earlier
     vectorized_review = tfidf_vectorizer.transform([cleaned_review])
 
     # make our prediction
     prediction = model_lr_best.predict(vectorized_review)
-    prob = model_lr_best.predict_proba(vectorized_review) # POTENTIALLY create table to show probabilities
+    prob = model_lr_best.predict_proba(vectorized_review)
 
     # retrieve new shap values to showcase prediction
     shap_value = explainer.shap_values(vectorized_review)
@@ -83,7 +98,7 @@ if(st.button('Predict') and review_input is not None):
         feature_names=tfidf_vectorizer.get_feature_names_out()
     )
 
-    shap.waterfall_plot(explanation, max_display=15, show=False)
+    shap.waterfall_plot(explanation, max_display=15, show=False) # display the top 15 features / 'words' which are contributing
 
     st.pyplot(fig)
     plt.clf()
